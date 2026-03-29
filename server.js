2294bb8f0e8009089ffc334d20a0f50cf6239258
Yogesh <Yogesh@SirishaeddysMBP.lan>
2026-03-29 00:11:55 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,70 @@
+require("dotenv").config();
+
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+
+const wp = axios.create({
+  baseURL: process.env.WP_URL + "/wp-json/wp/v2",
+  auth: {
+    username: process.env.WP_USERNAME,
+    password: process.env.WP_APP_PASSWORD,
+  },
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.post("/create-draft", async (req, res) => {
+  try {
+    const { title, content } = req.body;
+
+    const response = await wp.post("/posts", {
+      title: title,
+      content: content,
+      status: "draft",
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({
+      error: error.response?.data || error.message,
+    });
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running on port " + (process.env.PORT || 3000));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
